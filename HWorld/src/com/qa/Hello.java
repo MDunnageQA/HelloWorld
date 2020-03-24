a766226f7fd384c221805b5a8f8553254f23dd0e
MDunnageQA <62284099+MDunnageQA@users.noreply.github.com>
2020-03-24 01:17:45 +0000

    Output “Hello World!” to the console.

---
+++ HWorld/src/com/qa/Hello.java
@@ -0,0 +1,8 @@
+package com.qa;
+
+public class Hello {
+	public void print() {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ HWorld/src/com/qa/Main.java
@@ -0,0 +1,11 @@
+package com.qa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hello = new Hello();
+		hello.print();
+	}
+
+}
